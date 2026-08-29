802d754e9f1939d30bff7fa7fdcf232ad4b863e3
Noah Moss <noahbmoss@gmail.com>
2026-08-29 16:37:11 -0400

    extract handlers mod

--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -3,85 +3,12 @@ use std::{
     sync::{Arc, Mutex},
 };
 
-use async_trait::async_trait;
-use howdah_core::{QueryResult, run_query};
-use nvim_rs::{Handler, Neovim, Value, compat::tokio::Compat, create::tokio as create};
-use tokio::fs::File;
-use tokio_postgres::{Client, NoTls};
+use nvim_rs::create::tokio as create;
+use tokio_postgres::NoTls;
 
-#[derive(Clone, Debug)]
-struct NeovimHandler {
-    client: Arc<Mutex<Option<Arc<Client>>>>,
-}
+use handlers::NeovimHandler;
 
-#[async_trait]
-impl Handler for NeovimHandler {
-    type Writer = Compat<File>;
-
-    async fn handle_request(
-        &self,
-        name: String,
-        args: Vec<Value>,
-        _neovim: Neovim<Compat<File>>,
-    ) -> Result<Value, Value> {
-        match name.as_ref() {
-            "ping" => Ok(Value::from("pong")),
-            "query" => {
-                if args.len() == 1 {
-                    if let Some(sql) = args[0].as_str() {
-                        let client = self.client.lock().unwrap().as_ref().map(Arc::clone);
-                        match client {
-                            Some(client) => match run_query(&client, sql).await {
-                                Ok(QueryResult { rows, cols }) => {
-                                    let col_value =
-                                        Value::Array(cols.into_iter().map(Value::from).collect());
-
-                                    let row_values = Value::Array(
-                                        rows.into_iter()
-                                            .map(|row| {
-                                                Value::Array(
-                                                    row.into_iter().map(Value::from).collect(),
-                                                )
-                                            })
-                                            .collect(),
-                                    );
-                                    Ok(Value::Map(vec![
-                                        (Value::from("cols"), col_value),
-                                        (Value::from("rows"), row_values),
-                                    ]))
-                                }
-                                Err(err) => {
-                                    let mut err_text = format!("Execution error: {}\n", err);
-                                    let mut source = err.source();
-                                    while let Some(e) = source {
-                                        err_text.push_str(&format!("Caused by: {}\n", e));
-                                        source = e.source()
-                                    }
-                                    Err(Value::from(err_text))
-                                }
-                            },
-                            None => Err(Value::from(
-                                "not connected to a database (call connect() first)",
-                            )),
-                        }
-                    } else {
-                        Err(Value::from(format!(
-                            "method {} expects string, received {}",
-                            name, &args[0]
-                        )))
-                    }
-                } else {
-                    Err(Value::from(format!(
-                        "method {} expects 1 arg, received {}",
-                        name,
-                        args.len()
-                    )))
-                }
-            }
-            _ => Err(Value::from(format!("unknown method: {}", name))),
-        }
-    }
-}
+mod handlers;
 
 // TODO: add error logging not to stderr
 #[tokio::main]
