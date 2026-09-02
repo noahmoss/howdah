8e63d433722b758267562196728a63e058ba63b8
Noah Moss <noahbmoss@gmail.com>
2026-09-01 22:20:30 -0400

    add subcommands

--- plugin/howdah.lua
+++ plugin/howdah.lua
@@ -1,3 +1,35 @@
+local subcommands = {
+	open = {
+		impl = function(arg)
+			require("howdah").open(arg)
+		end,
+	},
+	start = {
+		impl = function()
+			require("howdah").start()
+		end,
+	},
+	connect = {
+		impl = function(arg)
+			require("howdah").connect(arg)
+		end,
+	},
+	stop = {
+		impl = function()
+			require("howdah").stop()
+		end,
+	},
+}
+
 vim.api.nvim_create_user_command("Howdah", function(opts)
-	require("howdah").open(opts.args)
+	local verb, rest = opts.args:match("^(%S*)%s*(.*)$")
+	if verb == "" then
+		verb = "open"
+	end
+	local subcommand = subcommands[verb]
+	if not subcommand then
+		vim.notify("Howdah: unknown subcommand '" .. verb .. "'", vim.log.levels.ERROR)
+		return
+	end
+	subcommand.impl(rest)
 end, { nargs = "*" })
